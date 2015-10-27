d812631a337bcb3372b1e9f82e3ae10b25b5cb03
maxim <soulman.is.good@gmail.com>
2015-10-27 11:27:40 +0600

    correct HEAD commit hash

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 process.title = "gitpub";
 
 var fs = require('fs');
-var path = require('path');
+var execSync = require('child_process').execSync;
 var express = require('express');
 var bodyParser = require('body-parser');
 var flow = require('./flow');
@@ -25,7 +25,7 @@ app.post('/', function (req, res, next) {
       if(data.repository.name === name) {
         var repo = config[name];
         for(var branch in repo) {
-          var current = fs.readFileSync(path.join(repo[branch].dir, "/.git/ORIG_HEAD"))
+          var current = execSync('cd "' + repo[branch].dir '" && git rev-parse HEAD')
             .toString().replace(/^\s+|\s+$/g, '');
           if(data.ref.replace(/(.+\/)/,'')===branch && data.after !== current) {
             flow(repo[branch].beforePull, repo[branch].dir).then(function(so) {
