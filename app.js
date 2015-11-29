996258c634e047d2d59ee788d68983407fd5e756
Maxim Savin <soulman.is.good@gmail.com>
2015-11-29 13:41:54 +0600

    bitbucket support

--- app.js
+++ app.js
@@ -28,31 +28,43 @@ app.post('/', function (req, res, next) {
         for(var branch in repo) {
           var current = execSync('cd "' + repo[branch].dir + '" && git rev-parse HEAD')
             .toString().replace(/^\s+|\s+$/g, '');
-          if(data.ref.replace(/(.+\/)/,'')===branch && data.after !== current) {
+          var remote_branch, remote_commit, message;
+          //get branch name (github/bitbucket)
+          if(data.ref) {
+            remote_branch = data.ref.replace(/(.+\/)/, '');
+            remote_commit = data.commits[0].id;
+            message = data.commits[0].message;
+          }
+          if(data.push && data.push.changes && data.push.changes.length > 0  && data.push.changes[0].new && data.push.changes[0].new.type === "branch") {
+            remote_branch = data.push.changes[0].new.name;
+            remote_commit = data.push.changes[0].new.target.hash;
+            message = data.push.changes[0].new.target.message;
+          }
+          if(remote_branch===branch && remote_commit !== current) {
             (function(branch, name, repo){
               flow(repo[branch].beforePull, repo[branch].dir).then(function(so) {
                 so && console.log(so);
                 flow("git pull", repo[branch].dir, function(err, so){
                   if(err) {
                     console.log("=====================================");
-                    console.log(new Date());
-                    console.error(err);
+                    console.log(new Date(), name + "::" + branch, remote_commit, "failed! Commit message:", message);
+                    console.error(err.stack || err);
                   } else {
                     flow(repo[branch].afterPull, repo[branch].dir).then(function(so){
                       console.log("=====================================");
-                      console.log(new Date(), name + "::" + branch, "DONE!");
+                      console.log(new Date(), name + "::" + branch, remote_commit, "success! Commit message:", message);
                     }).catch(function(err){
                       console.log("=====================================");
-                      console.log(new Date());
-                      console.error(err || se);
+                      console.log(new Date(), name + "::" + branch, remote_commit, "failed! Commit message:", message);
+                      console.error(err.stack || err || se);
                       flow("git reset --hard " + current, repo[branch].dir);
                     });
                   }
                 });
               }).catch(function(err){
                 console.log("=====================================");
-                console.log(new Date());
-                console.error(err);
+                console.log(new Date(), name + "::" + branch, remote_commit, "failed! Commit message:", message);
+                console.error(err.stack || err);
               });
             }(branch, name, repo));
           } else {
